08dc1589bd1b0df223eeb18d316ea269d4ec18fc
TamasPinter <119883462+TamasPinter@users.noreply.github.com>
2023-01-22 13:49:51 -0500

    working on script and display

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+// define variables
+let startButton = document.querySelector("sQuiz");
+let quizContainer = document.getElementById("quiz");
+
+//set up questiosn in array bank
+let questions = [
+    question("text?", ["option1", "option2", "option3", "option4"], "correct answer"),
+    question("text?", ["option1", "option2", "option3", "option4"], "correct answer"),
+    question("text?", ["option1", "option2", "option3", "option4"], "correct answer"),
+    question("text?", ["option1", "option2", "option3", "option4"], "correct answer"),
+    question("text?", ["option1", "option2", "option3", "option4"], "correct answer"),
+    question("text?", ["option1", "option2", "option3", "option4"], "correct answer"),
+    question("text?", ["option1", "option2", "option3", "option4"], "correct answer"),
+    question("text?", ["option1", "option2", "option3", "option4"], "correct answer"),
+    question("text?", ["option1", "option2", "option3", "option4"], "correct answer"),
+    question("text?", ["option1", "option2", "option3", "option4"], "correct answer"),
+];
+
