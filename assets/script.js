e83f5a5b9f81e8eeb055b6dca9bceca5523e3966
TamasPinter <119883462+TamasPinter@users.noreply.github.com>
2023-01-22 18:29:19 -0500

    worked on more quiz function

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 // define variables
 let startButton = document.querySelector("sQuiz");
 let quizContainer = document.getElementById("quiz");
+let quizArea = document.getElementById("question");
+let secondsLeft = 60;
 
 //set up questiosn in array bank
 let questions = [
@@ -16,3 +18,25 @@ let questions = [
     question("text?", ["option1", "option2", "option3", "option4"], "correct answer"),
 ];
 
+function quiz(){
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
+
+//Display questions and choices
+function quizQuest() {
+    if(quiz.finished()) {
+        showTotal();
+    }
+    else {
+        quizArea.innerHTML = quiz.getQuestionIndex().text;
+        let choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            let quizArea = document.getElementbyId("choice" + 1);
+            quizArea.innerHTML = choices[i];
+            guess('butt' + i, choices[i]);
+        }
+
+    }
+}
