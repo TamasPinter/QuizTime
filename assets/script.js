34049e8b37747ed82162b783977d95ded04466ef
TamasPinter <119883462+TamasPinter@users.noreply.github.com>
2023-01-23 22:02:16 -0500

    debugging quiz function

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,150 @@
-// define variables
-let startButton = document.querySelector("sQuiz");
-let quizContainer = document.getElementById("quiz");
-let quizArea = document.getElementById("question");
-let secondsLeft = 60;
+let score = 0;
+let timeLeft = 60;
+let question = 0;
+let startQuiz = document.querySelector("sQuiz");
 
-//set up questiosn in array bank
-let questions = [
-    question("text?", ["option1", "option2", "option3", "option4"], "correct answer"),
-    question("text?", ["option1", "option2", "option3", "option4"], "correct answer"),
-    question("text?", ["option1", "option2", "option3", "option4"], "correct answer"),
-    question("text?", ["option1", "option2", "option3", "option4"], "correct answer"),
-    question("text?", ["option1", "option2", "option3", "option4"], "correct answer"),
-    question("text?", ["option1", "option2", "option3", "option4"], "correct answer"),
-    question("text?", ["option1", "option2", "option3", "option4"], "correct answer"),
-    question("text?", ["option1", "option2", "option3", "option4"], "correct answer"),
-    question("text?", ["option1", "option2", "option3", "option4"], "correct answer"),
-    question("text?", ["option1", "option2", "option3", "option4"], "correct answer"),
+
+let questionsArray = [
+    //10 questions with answer for arrays
+    {
+      question: "What does parseInt do?",
+      choice1: "parses a string argument and returns a floating point number.",
+      choice2: "evaluates JavaScript code represented as a string.",
+      choice3:"parses a string argument and returns an integer of the specified radix",
+      choice4: "it pops up a prompt window in your browser.",
+      answer: "parses a string argument and returns an integer of the specified radix",
+    },
+    {
+      question: "how do you declare a variable?",
+      choice1: "variable name = value",
+      choice2: "var name = value;", 
+      choice3: "var.NAME = value;",
+      choice4: "var = name.value;",
+      answer: "var name = value;",
+    },
+    {
+      question: "which of these will evaluate to the number 20?",
+      choice1: "var twenty = 10 + 10; ",
+      choice2: "var twenty = '10' + '10'; ",
+      choice3: "var twenty = 10 + '10'; ",
+      choice4: "twenty = '20' ",
+      answer: "var twenty = 10 + 10; ",
+    },
+    {
+      question: "what is the proper syntax for this function?",
+      choice1: "myfunction() (console.log('Hello World!'); ",
+      choice2: "function MYFUNCTION {console.log('Hello World!'}; ",
+      choice3: "function myFunction() {console.log('Hello World!')}; ",
+      choice4: "function() = myFunction {console.log(12)} ",
+      answer: "function myFunction() {console.log('Hello World!')}; ",
+    },
+    {
+      question: "What does parseInt do?",
+      choice1: "parses a string argument and returns a floating point number.",
+      choice2: "evaluates JavaScript code represented as a string.",
+      choice3:"parses a string argument and returns an integer of the specified radix",
+      choice4: "it pops up a prompt window in your browser.",
+      answer: "parses a string argument and returns an integer of the specified radix",
+     },
+    {
+      question: "how do you declare a variable?",
+      choice1: "variable name = value",
+      choice2: "var name = value;", 
+      choice3: "var.NAME = value;",
+      choice4: "var = name.value;",
+      answer: "var name = value;",
+    },
+    {
+      question: "which of these will evaluate to the number 20?",
+      choice1: "var twenty = 10 + 10; ",
+      choice2: "var twenty = '10' + '10'; ",
+      choice3: "var twenty = 10 + '10'; ",
+      choice4: "twenty = '20' ",
+      answer: "var twenty = 10 + 10; ",
+    },
+     {
+      question: "what is the proper syntax for this function?",
+      choice1: "myfunction() (console.log('Hello World!'); ",
+      choice2: "function MYFUNCTION {console.log('Hello World!'}; ",
+      choice3: "function myFunction() {console.log('Hello World!')}; ",
+      choice4: "function() = myFunction {console.log(12)} ",
+      answer: "function myFunction() {console.log('Hello World!')}; ",
+    },
+    {
+      question: "What does parseInt do?",
+      choice1: "parses a string argument and returns a floating point number.",
+      choice2: "evaluates JavaScript code represented as a string.",
+      choice3:"parses a string argument and returns an integer of the specified radix",
+      choice4: "it pops up a prompt window in your browser.",
+      answer: "parses a string argument and returns an integer of the specified radix",
+    },
+    {
+      question: "how do you declare a variable?",
+      choice1: "variable name = value",
+      choice2: "var name = value;", 
+      choice3: "var.NAME = value;",
+      choice4: "var = name.value;",
+      answer: "var name = value;",
+    },
+    {
+      question: "which of these will evaluate to the number 20?",
+      choice1: "var twenty = 10 + 10; ",
+      choice2: "var twenty = '10' + '10'; ",
+      choice3: "var twenty = 10 + '10'; ",
+      choice4: "twenty = '20' ",
+      answer: "var twenty = 10 + 10; ",
+    },
+    {
+      question: "what is the proper syntax for this function?",
+      choice1: "myfunction() (console.log('Hello World!'); ",
+      choice2: "function MYFUNCTION {console.log('Hello World!'}; ",
+      choice3: "function myFunction() {console.log('Hello World!')}; ",
+      choice4: "function() = myFunction {console.log(12)} ",
+      answer: "function myFunction() {console.log('Hello World!')}; ",
+    },
 ];
 
-function quiz(){
-    this.score = 0;
-    this.questions = questions;
-    this.questionIndex = 0;
-}
+// create a function to call on question array
+function showQuestions() {
+    let object = questionArray[questionNumber];
+    let {question, choice1, choice2, choice3, choice4} = object;
+    let choices = [choice1, choice2, choice3, choice4];
 
-//Display questions and choices
-function quizQuest() {
-    if(quiz.finished()) {
-        showTotal();
-    }
-    else {
-        quizArea.innerHTML = quiz.getQuestionIndex().text;
-        let choices = quiz.getQuestionIndex().choices;
-        for(var i = 0; i < choices.length; i++) {
-            let quizArea = document.getElementbyId("choice" + 1);
-            quizArea.innerHTML = choices[i];
-            guess('butt' + i, choices[i]);
-        }
-
+    questionText.textContent = question;
+//iterate through choices with forloop
+    for (var i = 0; i < choicesArray.length; i++) {
+        let selection = choicesText[i];
+        let choice = choiceArray[i];
+        selection.innerHTML = choice;
     }
 }
+
+//start event
+startQuiz.addEventListener("click", function() {
+    showQuestions();
+    for (var i =0; i < 10; i++) {
+        let selection = choicesText[i];
+        selection.addEventListener("click", function(pick) {
+            
+            //register user selection from array
+            const userChoice = pick.currentTarget.textContent
+            const rightChoice = questionsArray[question].answer
+            //if answer is right, + point
+            if (userChoice === rightChoice) {
+                score++;
+            }
+
+            question++;
+            //when all questions run through quiz ends
+            if (question >= questionsArray.length) {
+                clearQuestion();
+                clearInterval(interval);
+                timer.innerHTML = "Quiz is Over";
+                return;
+            }
+            showQuestions();
+        });
+    }
+})
+
+//create timer
